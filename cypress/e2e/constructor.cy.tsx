cadbb67abf91ce551229f7570c3d5735bddaceed
Xenia <zhukovaxenia89@yandex>
2025-06-25 13:34:44 +0400

    Changed cypress: added baseUrl, custom commands

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,102 +1,97 @@
 describe('проверяем доступность приложения', () => {
-    it('сервис должен быть доступен по адресу localhost:4000', () => {
-        cy.visit('http://localhost:4000'); 
-    });
-}); 
+  it('сервис должен быть доступен по адресу localhost:4000', () => {
+    cy.visit('/');
+  });
+});
 
 describe('Добавление ингредиента из списка ингредиентов в конструктор', () => {
-    beforeEach(() =>{
-        cy.intercept('GET', 'api/ingredients', {fixture: 'ingredients.json'});
-        cy.viewport(1300, 800);
-        cy.visit('http://localhost:4000');
-    });
+  beforeEach(() => {
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+    cy.viewport(1300, 800);
+    cy.visit('/');
+  });
 
-    it('добавление булки', () => {
-        cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
-        cy.get('[data-cy=constructor-bun-1]')
-          .contains('Краторная булка 1')
-          .should('exist');
-        cy.get('[data-cy=constructor-bun-2]')
-          .contains('Краторная булка 1')
-          .should('exist');
-    })
+  it('добавление булки', () => {
+    cy.clickAddButtonForIngredient('bun-ingredients');
+    cy.getBySelectorText('[data-cy=constructor-bun-1]', 'Краторная булка 1');
+    cy.getBySelectorText('[data-cy=constructor-bun-2]', 'Краторная булка 1');
+  });
 
-    it('добавление ингредиента', () => {
-        cy.get('[data-cy=main-ingredients]').contains('Добавить').click();
-        cy.get('[data-cy=sauce-ingredients]').contains('Добавить').click();
-        cy.get('[data-cy=constructor-ingredients]')
-          .contains('Котлета 1')
-          .should('exist');
-        cy.get('[data-cy=constructor-ingredients]')
-          .contains('Соус 1')
-          .should('exist');
-    })
-})
+  it('добавление ингредиента', () => {
+    cy.clickAddButtonForIngredient('main-ingredients');
+    cy.clickAddButtonForIngredient('sauce-ingredients');
+    cy.getBySelectorText('[data-cy=constructor-ingredients]', 'Котлета 1');
+    cy.getBySelectorText('[data-cy=constructor-ingredients]', 'Соус 1');
+  });
+});
 
 describe('Открытие и закрытие модального окна с описанием ингредиента', () => {
-    beforeEach( () => {
-        cy.intercept('GET', 'api/ingredients', {fixture: 'ingredients.json'});
-        cy.viewport(1300, 800);
-        cy.visit('http://localhost:4000');
-    });
+  beforeEach(() => {
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+    cy.viewport(1300, 800);
+    cy.visit('/');
+  });
 
-    it('открытие модального окна', () => {
-        cy.contains('Детали ингредиента').should('not.exist');
-        cy.contains('Краторная булка 1').click();
-        cy.contains('Детали ингредиента').should('exist');
-//Отображение в открытом модальном окне данных именно того ингредиента, по которому произошел клик.
-        cy.get('#modals').contains('Краторная булка 1').should('exist');
-    })
+  it('открытие модального окна', () => {
+    cy.textDoesNotExist('Детали ингредиента');
+    cy.clickIngredientCard('Краторная булка 1');
+    cy.textExists('Детали ингредиента');
+    //Отображение в открытом модальном окне данных именно того ингредиента, по которому произошел клик.
+    cy.getBySelectorText('#modals', 'Краторная булка 1');
+  });
 
-    it('закрытие модального окна', () => {
-        cy.contains('Краторная булка 1').click();
-        cy.contains('Детали ингредиента').should('exist');
-        cy.get('[data-cy=modal-overlay]')
-            .click('left', {force: true});
-        cy.contains('Детали ингредиента').should('not.exist');
-    })
-})
+  it('закрытие модального окна', () => {
+    cy.clickIngredientCard('Краторная булка 1');
+    cy.textExists('Детали ингредиента');
+    cy.clickModalOverlay();
+    cy.textDoesNotExist('Детали ингредиента');
+  });
+});
 
 describe('Процесс создания заказа', () => {
-        beforeEach(() => {
-        cy.intercept('GET', 'api/ingredients', {fixture: 'ingredients.json'}).as('ingredients');
-        cy.intercept('GET', 'api/auth/user', {fixture: 'user.json'});
-        cy.intercept('POST', 'api/orders', {fixture: 'post_order.json'}).as('postOrder');
+  beforeEach(() => {
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
+      'ingredients'
+    );
+    cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
+    cy.intercept('POST', 'api/orders', { fixture: 'post_order.json' }).as(
+      'postOrder'
+    );
 
-        window.localStorage.setItem(
-            'refreshToken',
-            JSON.stringify('test-refreshToken')
-        );
-        cy.setCookie('accessToken', 'test-accessToken')
-        cy.viewport(1300, 800);
-        cy.visit('http://localhost:4000');
-    });
+    window.localStorage.setItem(
+      'refreshToken',
+      JSON.stringify('test-refreshToken')
+    );
+    cy.setCookie('accessToken', 'test-accessToken');
+    cy.viewport(1300, 800);
+    cy.visit('/');
+  });
 
-    afterEach(() => {
-        cy.clearLocalStorage();
-        cy.clearCookies();
-    })
+  afterEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+  });
 
-    it('добавление ингредиентов в конструктор бургера', () => {
-        cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
-        cy.get('[data-cy=main-ingredients]').contains('Добавить').click();
-        cy.get('[data-cy=sauce-ingredients]').contains('Добавить').click();
-        cy.get('[data-cy=order-summ] button').click();
+  it('добавление ингредиентов в конструктор бургера', () => {
+    cy.clickAddButtonForIngredient('bun-ingredients');
+    cy.clickAddButtonForIngredient('main-ingredients');
+    cy.clickAddButtonForIngredient('sauce-ingredients');
+    cy.clickOrderSummaryButton();
 
-        cy.wait('@postOrder')
-        .its('request.body')
-        .should('deep.equal', {
-            ingredients: ['1', '3', '5', '1']
-        });
-        //проверка отображения модального окна с верным номером заказа при клике на кнопку оформления заказа. 
-        cy.get('[data-cy=order-number]').contains('12').should('exist');
+    cy.wait('@postOrder')
+      .its('request.body')
+      .should('deep.equal', {
+        ingredients: ['1', '3', '5', '1']
+      });
+    //проверка отображения модального окна с верным номером заказа при клике на кнопку оформления заказа.
+    cy.getBySelectorText('[data-cy=order-number]', '12');
 
-        cy.get('#modals button[aria-label="Закрыть"]').click();
-        cy.get('[data-cy=order-number]').should('not.exist');
+    cy.clickModalCloseButton();
+    cy.elementDoesNotExist('[data-cy=order-number]');
 
-        //Проверка очистки конструктора бургера от добавленных ингредиентов.
-        cy.contains('Краторная булка 1').should('not.exist')
-        cy.contains('Котлета 1').should('not.exist');
-        cy.contains('Соус 1').should('not.exist')
-    })
-})
+    //Проверка очистки конструктора бургера от добавленных ингредиентов.
+    cy.textDoesNotExist('Краторная булка 1');
+    cy.textDoesNotExist('Котлета 1');
+    cy.textDoesNotExist('Соус 1');
+  });
+});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,75 @@
 //     }
 //   }
 // }
+
+declare namespace Cypress {
+  interface Chainable<Subject> {
+    //Проверяет, что элемент, содержащий указанный текст, не существует в DOM.
+    textDoesNotExist(text: string): Chainable<void>;
+
+    //Проверяет, что элемент, содержащий указанный текст, существует в DOM.
+    textExists(text: string): Chainable<HTMLElement>;
+
+    //Кликает по дочернему элементу, содержащему указанный текст,
+    clickAddButtonForIngredient(
+      containerDataCy: string
+    ): Chainable<HTMLElement>;
+
+    // Находит элемент по любому CSS-селектору и проверяет, что он содержит указанный текст.
+    getBySelectorText(selector: string, text: string): Chainable<JQuery<HTMLElement>>;
+
+    // Кликает по кнопке закрытия модального окна.
+    clickModalCloseButton(): Chainable<HTMLElement>;
+
+    //Кликает по кнопке внутри элемента сводки заказа.
+    clickOrderSummaryButton(): Chainable<HTMLElement>;
+
+    // Кликает по оверлею модального окна, используя принудительный клик.
+    clickModalOverlay(): Chainable<HTMLElement>;
+
+    //Кликает по карточке ингредиента, чтобы открыть его детали в модальном окне.
+    clickIngredientCard(ingredientName: string): Chainable<HTMLElement>;
+
+    // Проверяет, что элемент, найденный по указанному CSS-селектору, НЕ существует в DOM
+    elementDoesNotExist(selector: string): Chainable<void>;
+  }
+}
+// Команда для проверки отсутствия текста
+Cypress.Commands.add('textDoesNotExist', (text: string) => {
+  cy.contains(text).should('not.exist');
+});
+// Команда для проверки наличия текста
+Cypress.Commands.add('textExists', (text: string) => {
+  cy.contains(text).should('exist');
+});
+
+Cypress.Commands.add(
+  'clickAddButtonForIngredient',
+  (containerDataCy: string) => {
+    cy.get(`[data-cy=${containerDataCy}]`).contains('Добавить').click();
+  }
+);
+
+Cypress.Commands.add('getBySelectorText', (selector: string, text: string) => {
+  return cy.get(selector).should('contain', text);
+});
+
+Cypress.Commands.add('clickModalCloseButton', () => {
+  cy.get('#modals button[aria-label="Закрыть"]').click();
+});
+
+Cypress.Commands.add('clickOrderSummaryButton', () => {
+  cy.get('[data-cy=order-summ] button').click();
+});
+
+Cypress.Commands.add('clickModalOverlay', () => {
+  cy.get('[data-cy=modal-overlay]').click('left', { force: true });
+});
+
+Cypress.Commands.add('clickIngredientCard', (ingredientName: string) => {
+  cy.contains(ingredientName).click();
+});
+
+Cypress.Commands.add('elementDoesNotExist', (selector: string) => {
+  cy.get(selector).should('not.exist');
+});
